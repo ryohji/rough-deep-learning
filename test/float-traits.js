29c30bd224bb15b443f7bad1ef593501cbeec866
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-01-11 20:16:33 +0900

    Introduce node package definition.

---
+++ test/float-traits.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Floating point number', () => {
+    it('minimum is 5e-324.', () => {
+	const buffer = new ArrayBuffer(8);
+	const view = new DataView(buffer);
+	view.setUint8(7, 1);
+	assert.equal(5e-324, view.getFloat64(0));
+    });
+});
